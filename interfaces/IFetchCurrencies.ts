c4d64755571d74a1558f96e6cfd6cf013a26b575
Omid <54186037+heidariomid@users.noreply.github.com>
2022-12-25 23:00:28 +0330

    change how data filtered by using select in useQuery

--- interfaces/IFetchCurrencies.ts
+++ interfaces/IFetchCurrencies.ts
@@ -1,4 +1,4 @@
-import {QueryFunction} from 'react-query';
+import {QueryFunction} from '@tanstack/react-query';
 import {ICurrencyInfo} from './ICurrencyInfo';
 
 export type IfetchCurrencies = (page?: number, perPage?: number) => QueryFunction<ICurrencyInfo[]>;
